d0e32a6f73952d9ffb3586390af5651fefd90079
Shreemanto Lahiri <92041385+shreemantolahiri@users.noreply.github.com>
2022-03-08 12:27:46 +0530

    Add files via upload

---
+++ lane/contour_lane_approach.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+import cv2
+import math
+import rospy
+import numpy as np
+
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class send_it:
+
+  def __init__(self):
+    self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.callback)
+
+  def callback(self,data):
+    bridge = CvBridge()
+
+    try:
+      raw = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
+    except CvBridgeError as e:
+      rospy.logerr(e)
+    
+    image = raw.copy()
+    lower = np.uint8([190, 109, 109])
+    upper = np.uint8([255, 255, 255])
+    mask = cv2.inRange(image, lower, upper)
+    image = cv2.bitwise_and(image, image, mask = mask)
+
+    
+    # # HSV filter
+    # lower = np.array([0, 0, 200])
+    # upper = np.array([179, 30, 255])
+
+    # hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # mask = cv2.inRange(hsv, lower, upper)
+    # image = cv2.bitwise_and(image,image, mask= mask)
+    # print('hello')
+
+    # ROI masking
+    stencil = np.zeros_like(image[:,:,0])
+    polygon = np.array([[0,480],[0,250], [200,50], [440,50],[640,250] , [640,480]])
+
+    cv2.fillConvexPoly(stencil, polygon, 3)
+
+    image = cv2.bitwise_and(image, image, mask=stencil)
+
+    # contours
+    ret, thresh = cv2.threshold(image[:,:,0], 130, 145, cv2.THRESH_BINARY)
+    
+    contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)    
+
+    contours=[x for x in contours if len(x)> 200]
+
+    # lines
+
+    lines  = []
+
+    for contour in contours :
+      
+      vx,vy,x,y = cv2.fitLine(contour, cv2.DIST_L1,0,0.01,0.01)
+
+
+      theta = math.degrees(math.atan(vy/vx))
+
+      if theta < 0 :
+        theta = 180 + theta 
+
+      
+      if 200 < x and x < 440:
+        lines.append(theta)
+      
+
+    # controls
+    pub = rospy.Publisher('/cmd_vel',Twist,queue_size=10)
+
+    
+    
+
+    vel = Twist()
+
+    vel.angular.z = 0
+
+    if len(lines) > 0 :
+      theta = round(sum(lines)/len(lines),3)
+
+      print(theta)
+
+      if 30 < theta and theta < 150:
+        vel.angular.z =  math.pi/2 * (90-theta)/90
+
+
+    vel.linear.x = 1.95
+
+
+    pub.publish(vel)
+    
+    cv2.drawContours(image=image, contours=contours, contourIdx=-1, color=(255,0 , 0), thickness=3, lineType=cv2.LINE_AA)
+
+
+    cv2.imshow("Dashcam View", image)
+
+    cv2.waitKey(3)
+
+def main():
+	send_it()
+	
+	try:
+		rospy.spin()
+	except KeyboardInterrupt:
+    # print('hi')
+    # rospy.Publisher('/cmd_vel',Twist,queue_size=10)
+		rospy.loginfo("Shutting down")
+    
+	
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    rospy.init_node('camera_read', anonymous=False)
+    main()
---
+++ lane/hough_lines_approach.py
@@ -0,0 +1,127 @@
+
+from __future__ import print_function
+from dis import dis
+import math
+
+from  geometry_msgs.msg import Twist
+import sys
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+from tf.transformations import euler_from_quaternion
+import numpy as np
+
+class image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("image_topic_2",Image,queue_size=1)
+    self.cmd_pub = rospy.Publisher("/cmd_vel",Twist,queue_size=1)
+    self.bridge = CvBridge()
+    self.vel = Twist()
+    self.vel.linear.x = 1
+    now = rospy.Time.now()
+    rate = rospy.Rate(10)
+    while rospy.Time.now() < now + rospy.Duration.from_sec(2):
+      self.cmd_pub.publish(self.vel)
+      rate.sleep()
+    self.image_sub = rospy.Subscriber("/camera/color/image_raw",Image,self.callback)
+    # self.theta1=0
+    # self.theta2=0
+    # self.theta3=0
+
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    image = (cv_image).copy()
+    
+    lower = np.uint8([190, 109, 109])
+    upper = np.uint8([255, 255, 255])
+    mask = cv.inRange(image, lower, upper)
+    image = cv.bitwise_and(image, image, mask = mask)
+    # image= image[77:581,80:600]
+    stencil = np.zeros_like(image[:,:,0])
+    polygon = np.array([[0,480],[0,250], [200,50], [440,50],[640,250] , [640,480]])
+
+    cv.fillConvexPoly(stencil, polygon, 1)
+
+    image = cv.bitwise_and(image, image, mask=stencil)
+
+    
+    canny=cv.Canny(image, 50, 150)
+    
+   
+    lines=cv.HoughLinesP(canny, rho=1, theta=np.pi/180, threshold=20, minLineLength=70, maxLineGap=70)
+    
+
+    
+    thetas=[]
+    if lines is not None:
+        for i in range(0, len(lines)):
+            l = lines[i][0]
+            slope = (l[3]-0)/(l[2]-0)
+            theta = math.degrees(math.atan(slope))
+            # theta = round(theta,2)
+            if theta < 0 :
+                    theta = 180 + theta
+            if 200<l[0] and l[0] < 440:
+                thetas.append(theta)
+            cv.line(image, (l[0], l[1]), (l[2], l[3]), (0,0,255), 10, cv.LINE_AA)
+    else:
+        print('No Lines right now!')
+    self.vel.angular.z = 0
+    if len(lines) > 0 :
+      theta = round(sum(thetas)/len(thetas),3)
+        
+      print(theta)
+
+      if 30 < theta and theta < 150:
+        self.vel.angular.z = 0.9*(math.pi/2 * (90-theta)/90)
+      # elif 39<theta<40:
+      #     self.counter+=1
+
+      # if self.counter==40:
+      #     self.vel.angular.z = 0.9*(math.pi/2 * (90-80)/90)
+      #     self.counter1+=1
+
+    
+      # if self.counter1==10:
+      #     self.counter1=0
+      #     self.counter=0    
+
+
+        
+    self.vel.linear.x = 1.25
+
+
+    self.cmd_pub.publish(self.vel)
+
+
+    
+
+    cv.imshow("Image window", image)
+    cv.waitKey(3)
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError as e:
+        print(e)
+        pass
+
+def main(args):
+  rospy.init_node('image_converter', anonymous=True)
+  ic = image_converter()
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
